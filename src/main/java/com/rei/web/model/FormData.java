21995d75a59ba7e5923cd4977f84261dced596ed
kichirinri <kichirinri@gmail.com>
2024-01-26 21:05:01 +0900

    homework
     homework

---
+++ src/main/java/com/rei/web/model/FormData.java
@@ -0,0 +1,29 @@
+package com.rei.web.model;
+
+import java.util.List;
+
+public class FormData {
+    private String textInput;
+    private String dropdown;
+    private List<String> checkbox;
+    
+	public String getTextInput() {
+		return textInput;
+	}
+	public void setTextInput(String textInput) {
+		this.textInput = textInput;
+	}
+	public String getDropdown() {
+		return dropdown;
+	}
+	public void setDropdown(String dropdown) {
+		this.dropdown = dropdown;
+	}
+	public List<String> getCheckbox() {
+		return checkbox;
+	}
+	public void setCheckbox(List<String> checkbox) {
+		this.checkbox = checkbox;
+	}
+    
+}
